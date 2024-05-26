a8d1b665a78fd7da62beb9b13022b43bda383759
Maryam Abkar <mankara@ncsu.edu>
2024-05-26 15:51:59 -0400

    added the window frame of the game screen using pancurses

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
-
 extern crate pancurses;
-use pancurses::{initscr, endwin};
-
-
+use pancurses::{endwin, initscr};
 
 fn main() {
     let window = initscr();
     window.printw("Hello RustHack");
     window.refresh();
+    let subwindow = window.derwin(20, 80, 2, 2).unwrap();
+    //let subwindow2 = window.subwin(20, 30, 10, 65).unwrap();
+
+    //window.resize(10, 20);
+    subwindow.printw("here is a box");
+
+    subwindow.border('|', '|', '-', '-', '+', '+', '+', '+');
+    // subwindow2.border('|', '|', '-', '-', '+', '+', '+', '+');
+
+    window.border('|', '|', '-', '-', '+', '+', '+', '+');
+
     window.getch();
     endwin();
 }
