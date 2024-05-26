b580bc1bb4f0b27dd271484ebcb629e9ae46f096
Maryam Abkar <mankara@ncsu.edu>
2024-05-26 16:46:54 -0400

    added a player entity, need to print it on the screen and make it move

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
 extern crate pancurses;
 use pancurses::{endwin, initscr};
 
+struct Monst {
+    health: i32,
+    power: i32,
+    ac: i32,
+    glyph: char,
+}
+
 fn main() {
     let window = initscr();
     window.printw("Hello RustHack");
     window.refresh();
     let subwindow = window.derwin(20, 80, 2, 2).unwrap();
     //let subwindow2 = window.subwin(20, 30, 10, 65).unwrap();
+
+    let player = Monst {
+        health: 10,
+        power: 1,
+        ac: 10,
+        glyph: '@',
+    };
 
     //window.resize(10, 20);
     subwindow.printw("here is a box");
@@ -17,5 +31,7 @@ fn main() {
     window.border('|', '|', '-', '-', '+', '+', '+', '+');
 
     window.getch();
+
+    //TODO see if you can add a glyph player that can walk around the screen
     endwin();
 }
