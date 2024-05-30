3d9d1ff19c3cbccbe59e68f531afb94413243e48
Maryam Abkar <mankara@ncsu.edu>
2024-05-30 19:15:34 -0400

    reorganized the main.rs file and adding comments

--- src/main.rs
+++ src/main.rs
@@ -12,17 +12,23 @@ struct GameState {
 }
 
 fn main() {
+    // initialized the current game state, making the dungeon
     let mut current_state = GameState {
         dungeon: Vec::new(),
     };
 
+    // initialize the window and and make the 80x20 game window
+    // and make the window in which the player's stats is shown
     let window = initscr();
-    window.printw("Hello RustHack");
-    //   window.refresh();
     let subwindow = window.derwin(20, 80, 2, 2).unwrap();
+    let player_window = window.derwin(5, 80, 23, 2).unwrap();
 
-    // construct the first level of the game
+    // specify the boarders for each of the player windows
+    subwindow.border('|', '|', '-', '-', '-', '-', '-', '-');
+    player_window.border('|', '|', '-', '-', '+', '+', '+', '+');
+    window.border('|', '|', '-', '-', '+', '+', '+', '+');
 
+    // construct the first level of the game with dots on every square
     let mut first_level = Level {
         depth: 0,
         annotations: String::new(),
@@ -30,6 +36,7 @@ fn main() {
         objects: Vec::new(),
     };
 
+    // make the player with the correct glyph and window
     let player = Monst {
         health: 10,
         power: 1,
@@ -39,23 +46,19 @@ fn main() {
         location: Position { x: 2, y: 3 },
     };
 
+    // put the player glyph on the first_level map
     first_level.map[player.location.x as usize][player.location.y as usize] = player.glyph;
 
-    //let subwindow2 = window.subwin(20, 30, 10, 65).unwrap();
+    // put the first level in the dungeon game state
     current_state.dungeon.push(first_level);
 
-    //window.resize(10, 20);
-    let player_window = window.derwin(5, 80, 23, 2).unwrap();
-
-    subwindow.border('|', '|', '-', '-', '-', '-', '-', '-');
-    player_window.border('|', '|', '-', '-', '+', '+', '+', '+');
-
+    // TODO: this is supposed to be a function that takes the current level and prints it to the
+    // screen,player's glyph. change this eventually.
     subwindow.mvaddch(player.location.x, player.location.y, player.glyph);
-    window.border('|', '|', '-', '-', '+', '+', '+', '+');
 
+    //TODO: this has to be a loop of getting the player's input and altering the game accordingly
     window.getch();
 
-    //TODO see if you can add a glyph player that can walk around the screen
     endwin();
     println!(
         "this is the struct representation of the first level {:#?}",
