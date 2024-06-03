f3e3925fcf1176c48ac157e366519277210c4df2
Maryam Abkar <mankara@ncsu.edu>
2024-06-02 23:15:25 -0400

    saving slight progress on getting player movement

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use arr_macro::arr;
 use items::Item;
 use map::*;
 use monst::Monst;
-use pancurses::{endwin, initscr, Window};
+use pancurses::{endwin, initscr, Input::Character, Window};
 
 struct GameState {
     dungeon: Vec<map::Level>,
@@ -21,6 +21,7 @@ fn main() {
     // initialize the window and and make the 80x20 game window
     // and make the window in which the player's stats is shown
     let window = initscr();
+    pancurses::noecho();
     let subwindow = window.derwin(20, 80, 5, 2).unwrap();
     let player_window = window.derwin(5, 80, 25, 2).unwrap();
     let message_window = window.derwin(3, 80, 1, 2).unwrap();
@@ -41,7 +42,7 @@ fn main() {
     };
 
     // make the player with the correct glyph and window
-    let player = Monst {
+    let mut player = Monst {
         health: 10,
         power: 1,
         ac: 10,
@@ -50,7 +51,7 @@ fn main() {
         location: Position { x: 2, y: 3 },
     };
 
-    //TODO: this should also be a function that takes objects and stores it in the current level
+    // TODO: this should also be a function that takes objects and stores it in the current level
     // put the player glyph on the first_level map
     first_level.map[player.location.x as usize][player.location.y as usize] = player.glyph;
 
@@ -62,8 +63,18 @@ fn main() {
     populate_board(&subwindow, current_state.dungeon.get(0).unwrap());
     subwindow.mvaddch(player.location.x, player.location.y, player.glyph);
 
-    //TODO: this has to be a loop of getting the player's input and altering the game accordingly
+    // TODO: this has to be a loop of getting the player's input and altering the game accordingly
     let input = window.getch().unwrap();
+
+    match input {
+        // remove player from former location
+        // level.map[player.location.x as usize]
+        Character('h') => player.location.x -= 1,
+        Character('l') => player.location.x += 1,
+        Character('j') => player.location.y -= 1,
+        Character('k') => player.location.x += 1,
+        _ => (),
+    }
 
     endwin();
     //debugging
@@ -74,23 +85,10 @@ fn main() {
     println!("this is the input {:#?}", input);
 }
 
-fn mvplayer(input: char, level: &mut Level, player: &mut Monst) {
-    match input {
-        // remove player from former location
-        // level.map[player.location.x as usize]
-        'h' => player.location.x -= 1,
-        'l' => player.location.x += 1,
-        'j' => player.location.y -= 1,
-        'k' => player.location.x += 1,
-        _ => (),
-    }
-}
-
 fn populate_board(subwindow: &Window, level: &Level) {
     for i in 1..19 {
         for j in 1..79 {
             subwindow.mvaddch(i, j, level.map[i as usize][j as usize]);
         }
     }
-    //subwindow.mvaddch(player.location.x, player.location.y, player.glyph);
 }
