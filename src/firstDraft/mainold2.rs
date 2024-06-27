767e08fb0b1897b7e21e59c601729fb00472754f
Maryam Abkar <mankara@ncsu.edu>
2024-06-27 10:18:22 -0400

    revamped the ui with ratatui

---
+++ src/firstDraft/mainold2.rs
@@ -0,0 +1,79 @@
+extern crate pancurses;
+use pancurses::{endwin, initscr, newwin, Input};
+
+fn main() {
+    // initialized the current game state, making the dungeon
+    // initialize the window and and make the 80x20 game window
+    // and make the window in which the player's stats is shown
+    let window = initscr();
+    pancurses::noecho();
+    window.refresh();
+    let mut subwindow = newwin(20, 80, 5, 2);
+    let mut player_window = newwin(5, 80, 25, 2);
+    let mut message_window = newwin(3, 80, 1, 2);
+
+    let mut msgs: Vec<String> = Vec::new();
+    // specify the boarders for each of the player windows
+    //window.border('|', '|', '-', '-', '+', '+', '+', '+');
+    subwindow.border('|', '|', '-', '-', '+', '+', '+', '+');
+    player_window.border('|', '|', '-', '-', '+', '+', '+', '+');
+    message_window.border('|', '|', '-', '-', '+', '+', '+', '+');
+
+    subwindow.refresh();
+    player_window.refresh();
+    message_window.refresh();
+
+    msgs.push(String::from("Welcome to RustHack!"));
+    message_window.mvprintw(1, 1, msgs.get(0).unwrap());
+    message_window.refresh();
+
+    // handle terminal resize events
+    loop {
+        let input = window.getch();
+        match input {
+            Some(Input::KeyResize) => {
+                pancurses::resize_term(0, 0);
+                //window.border('|', '|', '-', '-', '+', '+', '+', '+');
+                subwindow.erase();
+                player_window.erase();
+                message_window.erase();
+
+                //let (x, y) = window.get_max_yx();
+                subwindow.resize(0, 0);
+                player_window.resize(0, 0);
+                message_window.resize(0, 0);
+
+                // rewrite all window information here and borders
+                message_window.mvprintw(1, 1, msgs.get(0).unwrap());
+
+                player_window.mvprintw(1, 1, "test2!");
+                subwindow.mvprintw(1, 1, "test3!");
+
+                subwindow.border('|', '|', '-', '-', '+', '+', '+', '+');
+                player_window.border('|', '|', '-', '-', '+', '+', '+', '+');
+                message_window.border('|', '|', '-', '-', '+', '+', '+', '+');
+
+                //refresh all the windows so that the changes can be registered
+                subwindow.refresh();
+                player_window.refresh();
+                message_window.refresh();
+            }
+
+            Some(Input::Character('q')) => {
+                break;
+            }
+
+            _ => (),
+        }
+    }
+
+    // TODO: save the current game state before deleting the window
+    //
+    // ////////////////////////////////////////////////////////////
+
+    // delete windows and end the game
+    subwindow.delwin();
+    player_window.delwin();
+    message_window.delwin();
+    endwin();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,112 @@
-extern crate pancurses;
-use pancurses::{endwin, initscr, newwin, Input};
+mod tui;
 
-fn main() {
-    // initialized the current game state, making the dungeon
-    // initialize the window and and make the 80x20 game window
-    // and make the window in which the player's stats is shown
-    let window = initscr();
-    pancurses::noecho();
-    window.refresh();
-    let mut subwindow = newwin(20, 80, 5, 2);
-    let mut player_window = newwin(5, 80, 25, 2);
-    let mut message_window = newwin(3, 80, 1, 2);
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use std::io;
 
-    let mut msgs: Vec<String> = Vec::new();
-    // specify the boarders for each of the player windows
-    //window.border('|', '|', '-', '-', '+', '+', '+', '+');
-    subwindow.border('|', '|', '-', '-', '+', '+', '+', '+');
-    player_window.border('|', '|', '-', '-', '+', '+', '+', '+');
-    message_window.border('|', '|', '-', '-', '+', '+', '+', '+');
+use ratatui::{
+    prelude::*,
+    symbols::border,
+    widgets::{block::*, *},
+};
 
-    subwindow.refresh();
-    player_window.refresh();
-    message_window.refresh();
+#[derive(Debug, Default)]
+pub struct App {
+    exit: bool,
+}
 
-    msgs.push(String::from("Welcome to RustHack!"));
-    message_window.mvprintw(1, 1, msgs.get(0).unwrap());
-    message_window.refresh();
+impl App {
+    /// runs the application's main loop until the user quits
+    pub fn run(&mut self, terminal: &mut tui::Tui) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.render_frame(frame))?;
+            self.handle_events()?;
+        }
+        Ok(())
+    }
 
-    // handle terminal resize events
-    loop {
-        let input = window.getch();
-        match input {
-            Some(Input::KeyResize) => {
-                //pancurses::resize_term(0, 0);
-                //window.border('|', '|', '-', '-', '+', '+', '+', '+');
-                subwindow.erase();
-                player_window.erase();
-                message_window.erase();
+    fn render_frame(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.size());
+    }
 
-                //let (x, y) = window.get_max_yx();
-                subwindow.resize(0, 0);
-                player_window.resize(0, 0);
-                message_window.resize(0, 0);
-
-                // rewrite all window information here and borders
-                message_window.mvprintw(1, 1, msgs.get(0).unwrap());
-
-                player_window.mvprintw(1, 1, "test2!");
-                subwindow.mvprintw(1, 1, "test3!");
-
-                subwindow.border('|', '|', '-', '-', '+', '+', '+', '+');
-                player_window.border('|', '|', '-', '-', '+', '+', '+', '+');
-                message_window.border('|', '|', '-', '-', '+', '+', '+', '+');
-
-                //refresh all the windows so that the changes can be registered
-                subwindow.refresh();
-                player_window.refresh();
-                message_window.refresh();
+    fn handle_events(&mut self) -> io::Result<()> {
+        match event::read()? {
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event)
             }
+            _ => {}
+        };
 
-            Some(Input::Character('q')) => {
-                break;
-            }
+        Ok(())
+    }
 
-            _ => (),
+    fn handle_key_event(&mut self, key_event: KeyEvent) {
+        match key_event.code {
+            KeyCode::Char('Q') => self.exit(),
+            _ => {}
         }
     }
 
-    // TODO: save the current game state before deleting the window
-    //
-    // ////////////////////////////////////////////////////////////
+    fn exit(&mut self) {
+        self.exit = true;
+    }
+}
 
-    // delete windows and end the game
-    subwindow.delwin();
-    player_window.delwin();
-    message_window.delwin();
-    endwin();
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let layout = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints(vec![
+                Constraint::Percentage(10),
+                Constraint::Percentage(70),
+                Constraint::Percentage(20),
+            ])
+            .split(area);
+
+        let title = Title::from(Line::from("Dungeon").bold());
+        let msg_title = Title::from(Line::from("Messages").bold());
+        let player_info_title = Title::from(Line::from("Player").bold());
+
+        let instructions = Title::from(Line::from(vec!["Quit ".into(), "<Q>".yellow().bold()]));
+
+        let msg_block = Block::default()
+            .title(msg_title.alignment(Alignment::Left))
+            .borders(Borders::ALL)
+            .border_set(border::THICK);
+
+        let player_info_title_block = Block::default()
+            .title(player_info_title.alignment(Alignment::Left))
+            .borders(Borders::ALL)
+            .border_set(border::THICK);
+
+        let dungeon_block = Block::default()
+            .title(title.alignment(Alignment::Left))
+            .title(
+                instructions
+                    .alignment(Alignment::Center)
+                    .position(Position::Bottom),
+            )
+            .borders(Borders::ALL)
+            .border_set(border::THICK);
+
+        Paragraph::new("Welcome to RustHack!")
+            .centered()
+            .block(msg_block)
+            .render(layout[0], buf);
+        Paragraph::new("....@....")
+            .centered()
+            .block(dungeon_block)
+            .render(layout[1], buf);
+
+        Paragraph::new("Name:Default  PW:0  AC:0")
+            .centered()
+            .block(player_info_title_block)
+            .render(layout[2], buf);
+    }
+}
+
+fn main() -> io::Result<()> {
+    let mut terminal = tui::init()?;
+    let app_result = App::default().run(&mut terminal);
+    tui::restore()?;
+    app_result
 }
